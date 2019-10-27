d5a0df84ec5884835d70ec6078ced045161316e0
guohaoning1996 <1335806944@qq.com>
2019-10-27 21:20:25 +0800

    '登陆页面'

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,23 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '@/views/login/index.vue'
+import home from '@/views/home'
+import NotFound from '@/views/404'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [{
     path: '/login',
     component: Login
-  }]
+  },
+  {
+    path: '/',
+    component: home
+  },
+  {
+    path: '*',
+    component: NotFound
+  }
+  ]
 })
 export default router
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,20 +4,27 @@
     <el-card>
       <!-- 黑马头条  图片 -->
       <img src="../../assets/logo_index.png" width="200px" alt />
-      <!-- 输入框 -->
-      <el-form ref="form" :model="form">
-        <el-form-item>
-          <el-input v-model="form.phone" placeholder="请输入手机号"></el-input>
+      <!--
+        输入框
+
+      -->
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" status-icon>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input v-model="form.code" placeholder="请输入验证码" style="width:235px;margin-right:10px"></el-input>
-          <el-button>发送验证码</el-button>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:235px;margin-right:10px"
+          ></el-input>
+          <el-button type="danger" plain style="width:115px">发送验证码</el-button>
         </el-form-item>
         <el-form-item>
           <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" style="width:100%">立即登录</el-button>
+          <el-button @click="login" type="danger" style="width:100%">立即登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -27,11 +34,49 @@
 <script>
 export default {
   data () {
-    return {
-      form: {
-        phone: '',
-        code: ''
+    // 校验手机号的函数
+    const checkMobile = (rule, value, callback) => {
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号格式不对'))
       }
+    }
+
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '验证码6个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs['loginForm'].validate(valid => {
+        if (valid) {
+          // 发请求 校验手机号和验证码  后台
+          this.$http
+            .post('authorizations', this.loginForm)
+            .then(res => {
+              // 成功
+              this.$router.push('/')
+            })
+            .catch(() => {
+              // 失败 提示
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
     }
   }
 }
@@ -51,6 +96,7 @@ export default {
   width: 400px;
   height: 350px;
   position: absolute;
+  opacity: 0.7;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
